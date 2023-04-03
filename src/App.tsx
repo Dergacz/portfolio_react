5ce99616c18d4c5c60490018027afc8075f6e7c5
Stanislau Dziarhach <sats96dergach@gmail.com>
2023-04-03 19:38:59 +0200

    add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import '../public/sass/main.scss';
-import {Home} from "./pages/Home/Home";
+import { Home } from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-        <Home/>
-        <div className="container">
-
-        </div>
+      <Home />
+      <div className="container"></div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
