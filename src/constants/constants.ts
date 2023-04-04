82482a90e98b543f38876b48bccf8e4f4f98d288
Stanislau Dziarhach <sats96dergach@gmail.com>
2023-04-04 16:20:21 +0200

    add constants

---
+++ src/constants/constants.ts
@@ -0,0 +1,8 @@
+export const NAVBAR_ITEMS = [
+  { title: 'Home', link: '/home' },
+  { title: 'About', link: '/about' },
+  { title: 'What I Do', link: '/whatIdo' },
+  { title: 'Resume', link: '/resume' },
+  { title: 'Portfolio', link: '/portfolio' },
+  { title: 'Contact', link: '/contact' }
+];
