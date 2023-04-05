552a09988bd60425333178ccd9c61b416b61642d
Stanislau Dziarhach <sats96dergach@gmail.com>
2023-04-05 14:55:32 +0200

    add skills component

---
+++ src/pages/Skills/Skills.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Header } from '../../components/Header/Header';
+import { Skill } from '../../components/Skill/Skill';
+import * as constants from '../../constants/constants';
+import { IconContext } from 'react-icons';
+
+export const Skills = () => {
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <div className="skills">
+          <Header title="services" subtitle="what I do" />
+          <IconContext.Provider value={{ size: '40px', color: '#20c997' }}>
+            <div className="skills-wrapper">
+              {constants.SKILLS_ITEMS.map((skill) => {
+                return (
+                  <Skill
+                    title={skill.title}
+                    description={skill.description}
+                    icon={skill.icon}
+                  />
+                );
+              })}
+            </div>
+          </IconContext.Provider>
+        </div>
+      </div>
+    </div>
+  );
+};
