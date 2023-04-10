e751c9f9092017047cc5fdb595f709a6ef130da3
Stanislau Dziarhach <sats96dergach@gmail.com>
2023-04-10 10:45:23 +0200

    add link from react scroll

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import * as constants from '../../constants/constants';
+import { Link } from 'react-scroll';
 
 export const Navbar = () => {
   return (
     <ul className="navbar">
-      {
-        constants.NAVBAR_ITEMS.map(item => {
-          return (
-            <li className="navbar-item">
-              <a className="navbar-link" href={item.link}>
-                {item.title}
-              </a>
-            </li>
-          );
-        })
-      }
+      {constants.NAVBAR_ITEMS.map((item) => {
+        return (
+          <li className="navbar-item" key={item.title}>
+            <Link
+              className="navbar-link"
+              href="#"
+              to={item.link}
+              smooth={true}
+              duration={1000}
+            >
+              {item.title}
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 };
