bc47d1d3ea0f3a7c223b4af659affda08f200792
Stanislau Dziarhach <sats96dergach@gmail.com>
2023-04-11 12:49:34 +0200

    Fix navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { FC } from 'react';
 import * as constants from '../../constants/constants';
 import { Link } from 'react-scroll';
+import { INavbar } from './Navnar.types';
 
-export const Navbar = () => {
+export const Navbar: FC<INavbar> = ({ scroll }) => {
   return (
-    <ul className="navbar">
+    <ul className={`navbar ${scroll >= 200 ? 'navbar-fixed' : ''}`}>
       {constants.NAVBAR_ITEMS.map((item) => {
         return (
           <li className="navbar-item" key={item.title}>
             <Link
               className="navbar-link"
+              isDynamic={true}
               href="#"
+              spy={true}
               to={item.link}
               smooth={true}
+              delay={100}
               duration={1000}
+              offset={-80}
             >
               {item.title}
             </Link>
