47fbb282a8ee33af4f51eb59bd2f5ab71da3c871
Stanislau Dziarhach <sats96dergach@gmail.com>
2023-04-29 17:14:47 +0200

    add polish language

--- src/i18next.ts
+++ src/i18next.ts
@@ -1,27 +1,23 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import { englishLanguage } from './languages/en/en';
+import { polishLanguage } from './languages/pl/pl';
 
-i18n
-  .use(initReactI18next)
-  .init({
-    resources: {
-      en: {
-        translation: {
-          'home.hello': 'Welcome',
-        },
-      },
-      pl: {
-        translation: {
-          'home.hello': 'Witam',
-        },
-      }
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: englishLanguage,
     },
-    lng: 'en',
-    fallbackLng: 'en',
-    debug: true,
-    interpolation: {
-      escapeValue: false,
+    pl: {
+      translation: polishLanguage,
     },
-  });
+  },
+  lng: 'en',
+  fallbackLng: 'en',
+  debug: true,
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
---
+++ src/languages/pl/pl.ts
@@ -0,0 +1,148 @@
+export const polishLanguage = {
+  'navbar': {
+    'home': 'Home',
+    'about': 'O Mnie',
+    'whatIdo': 'Co Robię',
+    'resume': 'Moje CV',
+    'portfolio': 'Portfolio',
+    'contact': 'Kontakt',
+  },
+  'home': {
+    'hello': 'Witam',
+    'title': {
+      'iam': 'Jestem',
+      'name': 'Mam na imię Stanislau Dziarhach.',
+      'profession': {
+        'first': 'Jestem Frontend Developerem.',
+        'second': 'Jestem Inżynierem Oprogramowania.',
+      }
+    },
+    'based': 'mieszkam w Polsce, w Gdańsku',
+    'button': 'Napisz Do Mnie',
+    'photo': '2'
+  },
+  'about': {
+    'header': {
+      'title': 'o mnie',
+      'subtitle': 'poznaj mnie lepiej'
+    },
+    'iam': 'Mam na imię',
+    'title': 'jestem Frontend Developerem',
+    'description_1': 'Jestem doświadczonym Frontend developerem z 2,5 letnim doświadczeniem zawodowym. Posiadam umiejętności w zakresie HTML, CSS, JavaScript i frameworków takich jak React, a także biegłość w używaniu Git. Znam biegle kilka języków i codziennie się doskonalę.',
+    'description_2': 'Moim celem jest pomóc Państwu wprowadzić w życie wszystkie pomysły. Wierzę, że moje umiejętności i doświadczenie zawodowe mogą wnieść cenny wkład w realizację Państwa celów biznesowych. W swoich projektach wykorzystuję nowe narzędzia informatyczne, a jeśli pojawiają się trudności, pokonuję je z łatwością. Zawsze zorientowany na wyniki - to moje motto.',
+    'info': {
+      'name': 'Imie',
+      'age': 'Wiek',
+      'from': 'Mieszkam w',
+      'city': 'Gdańsk, Polska'
+    },
+    'download_btn': 'Pobierz CV'
+  },
+  'skills': {
+    'header': {
+      'title': 'serwisy',
+      'subtitle': 'co robię'
+    },
+    'skill': {
+      'react': {
+        'title': 'react',
+        'description': 'Tworzę nowoczesne aplikacje z wykorzystaniem React.'
+      },
+      'redux': {
+        'title': 'redux',
+        'description': 'W swoich projektach używam state managera redux. Mam komercyjne doświadczenie z Redux-saga, Redux-toolkit.'
+      },
+      'sass': {
+        'title': 'SASS',
+        'description': 'Piszę style za pomocą nowoczesnych preprocesorów.'
+      },
+      'type_script': {
+        'title': 'TypeScript',
+        'description': 'Używam TypeScript dla lepszej czytelności rozszerzalności kodu.'
+      },
+      'jest': {
+        'title': 'Jest',
+        'description': 'Wszystkie moje projekty pokrywam testami.'
+      },
+      'team_player': {
+        'title': 'gracz zespołowy',
+        'description': 'Lubię pracę zespołową, uważam, że tylko w zespole można osiągnąć wielki sukces.'
+      },
+    }
+  },
+  'resume': {
+    'header': {
+      'title': 'podsumowanie',
+      'subtitle': 'resume'
+    },
+    'education': {
+      'title': 'Moje wykształcenie',
+      'incubator': {
+        'title': 'Frontend developer',
+        'subtitle': 'IT-Incubator',
+        'description': 'Props (attributes), functional Components, class Components, state, methods of lifecycle, create-react-app, bind, Babel, ECMAScript, presentational & containerComponents, redux-thunk, state vs Redux, HOC, React Hooks, useReducer, useMemo, useCallback, FLAX, npm/yarn.'
+      },
+      'bru': {
+        'title': 'Inżynier Automatyki',
+        'subtitle': 'Uniwersytet Białorusko-Rosyjski',
+        'description': 'Zapoznałem się z takimi tematami jak: historia i stan obecny automatyzacji produkcji, główne środki techniczne automatyzacji, zasady i metody projektowania systemów zautomatyzowanych, programowanie w językach wysokiego poziomu, teoria obwodów elektrycznych, tworzenie systemów sterowania robotami oraz organizacja operacji produkcyjnych.'
+      }
+    },
+    'work': {
+      'title': 'Moje doświadczenie',
+      'itechart': {
+        'title': 'Inżynier oprogramowania',
+        'subtitle': 'iTechArt',
+        'description': 'Stworzyłem kilka aplikacji internetowych przy użyciu React, Redux i Redux Toolkit, aby stworzyć skalowalne i responsywne interfejsy użytkownika. Używałem Webpack i Vite do łączenia i optymalizacji kodu front-end. Tworzyłem komponenty UI przy użyciu NextUI i optymalizowałem je dla różnych urządzeń.'
+      },
+      'clarity_project': {
+        'title': 'Programista Frontend',
+        'subtitle': 'Clarity Project',
+        'description': 'Pracowałem nad kilkoma projektami wykorzystującymi React, Redux i Redux Saga do tworzenia skalowalnych i responsywnych aplikacji internetowych. Pracowałem z zespołami cross-funkcjonalnymi, aby wdrożyć nowe funkcje i zoptymalizować istniejącą bazę kodu. Opracowałem i utrzymywałem testy jednostkowe, aby zapewnić jakość kodu i zapobiec regresjom. Używałem Git i Bitbucket do kontroli wersji oraz Jira do zarządzania projektami. Zaprojektowałem i zaimplementowałem komponenty UI używając Material UI i Figma. Tworzyłem historie i rozwijałem komponenty przy użyciu Storybook, aby zwiększyć użyteczność i współpracę.'
+      }
+    },
+    'skills': {
+      'title': 'Moje umiejętności',
+      'item': {
+        'react': 'React',
+        'java_script': 'JavaScript',
+        'types_script': 'TypesScript',
+        'redux': 'Redux',
+        'html_css': 'HTML/CSS',
+        'jest': 'Jest',
+        'git': 'Git',
+        'webpack': 'Webpack',
+        'english': 'Angielski',
+        'polish': 'Polski',
+      }
+    },
+    'download_btn': 'Pobierz CV'
+  },
+  'portfolio': {
+    'header': {
+      'title': 'portfolio',
+      'subtitle': 'moje projekty'
+    },
+  },
+  'contact': {
+    'header': {
+      'title': 'kontakt',
+      'subtitle': 'skontaktować się'
+    },
+    'title': 'SKONTAKTUJMY SIĘ',
+    'item': {
+      'number': '+48791112379',
+      'email': 'stanislav.dziarhach@gmail.com',
+      'city': 'Gdańsk, Polska'
+    },
+    'form': {
+      'title': 'wyślij mi wiadomość',
+      'item': {
+        'name': 'Imię',
+        'email': 'Email',
+        'message': 'Twoja wiadomość'
+      }
+    },
+    'send_btn': 'Wyślij wiadomość'
+  }
+};
