90726b4fdaa4dbf225d95552d01e92d3bc919aef
Stanislau Dziarhach <sats96dergach@gmail.com>
2023-05-18 15:52:32 +0200

    fix navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,13 +6,20 @@ import { RxHamburgerMenu, VscChromeClose } from 'react-icons/all';
 import { IconContext } from 'react-icons';
 import { Languages } from '../Languages/Languages';
 import { useTranslation } from 'react-i18next';
+import { SocialMedia } from '../SocialMedia/SocialMedia';
 
 export const Navbar: FC<INavbar> = ({ scroll }) => {
   const { t } = useTranslation();
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
-    <div className={'navbar '}>
+    <div className="navbar">
+      <Languages isSelectOpen={isOpen} scroll={scroll} />
+      <IconContext.Provider value={{ color: '#20c997', size: '25px' }}>
+        <div className="navbar-button" onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? <VscChromeClose /> : <RxHamburgerMenu />}
+        </div>
+      </IconContext.Provider>
       <ul
         className={`navbar-items ${scroll >= 200 ? 'navbar-fixed' : ''} ${
           isOpen ? 'navbar-show' : ''
@@ -40,12 +47,9 @@ export const Navbar: FC<INavbar> = ({ scroll }) => {
           );
         })}
       </ul>
-      <Languages isSelectOpen={isOpen} />
-      <IconContext.Provider value={{ color: '#20c997', size: '25px' }}>
-        <div className="navbar-button" onClick={() => setIsOpen(!isOpen)}>
-          {isOpen ? <VscChromeClose /> : <RxHamburgerMenu />}
-        </div>
-      </IconContext.Provider>
+      <div className={`navbar-media ${scroll >= 200 ? 'navbar-fixed' : ''}`}>
+        <SocialMedia />
+      </div>
     </div>
   );
 };
